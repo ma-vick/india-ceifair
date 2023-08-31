f615bd4ca23a04eedf3ea5f69313a73563e34096
Victória Almeida <106677669+ma-vick@users.noreply.github.com>
2023-08-31 00:21:45 +0000

    cei fair page

---
+++ src/pages/AiIndia/AiIndia.jsx
@@ -0,0 +1,11 @@
+import MainContainer from "../../components/MainContainer/MainContainer";
+
+export default function AiIndia(){
+    const content = (
+        <section>
+            <h1>Ai in India</h1>
+        </section>
+    );
+
+    return <MainContainer childrn={content} />;
+}
